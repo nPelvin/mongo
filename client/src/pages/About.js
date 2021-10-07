b924702e83159d367b3f02e8e773cd9ac09eb7e0
nPelvin <pelvinpt@gmail.com>
2021-10-07 12:13:08 +0100

    First commit

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,7 +1,7 @@
 const About = () => (
 	<main role="main">
 		<div>
-			<h1>About</h1>
+			<h1>Test site for Mongo</h1>
 			<p>
 				Starter kit for full-stack JavaScript projects. For more information,
 				see the wiki:

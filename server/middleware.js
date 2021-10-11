2829ad854557a6f96f0ce18265f67537b892c716
nPelvin <pelvinpt@gmail.com>
2021-10-11 16:23:35 +0100

    fixing heroku take 2
    fixing heroku take 2
    updated content security policy in react middleware

--- server/middleware.js
+++ server/middleware.js
@@ -7,7 +7,7 @@ export const configuredHelmet = () =>
 			directives: {
 				defaultSrc: ["'self'"],
 				objectSrc: ["'none'"],
-				scriptSrc: ["'self'", "unpkg.com", "polyfill.io"],
+				scriptSrc: ["'self'", "unpkg.com", "polyfill.io", "guarded-dusk-77491.herokuapp.com"],
 				styleSrc: ["'self'", "https: 'unsafe-inline'"],
 				upgradeInsecureRequests: [],
 			},
